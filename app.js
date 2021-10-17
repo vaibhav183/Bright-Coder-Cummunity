505a686d903e65dde742b0c42871bbbfaae0ff28
sivs23 <shivamsourav2001@gmail.com>
2021-10-17 21:05:21 +0530

    some changes2

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'));
+const ejs = require('ejs');
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('blogindex');
+})
+
+app.listen(3000,()=>{
+    console.log("Server Reached at port 3000");
+})
